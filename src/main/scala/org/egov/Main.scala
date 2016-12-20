14cba0b8e73928332491c16c9219ac304010595d
jeado <jeado@bitfinder.co>
2016-12-20 22:09:01 +0900

    add router

--- src/main/scala/org/egov/Main.scala
+++ src/main/scala/org/egov/Main.scala
@@ -1,26 +1,32 @@
 package org.egov
 
 import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.routing.ConsistentHashingRouter.ConsistentHashableEnvelope
+import akka.routing.FromConfig
+import akka.util.Timeout
 import org.egov.RuleEngine._
+
+import scala.concurrent.Await
 
 /**
   * Created by gojaedo on 2016. 12. 19..
   */
 object Main extends App {
+  import akka.pattern.ask
+  import scala.concurrent.duration._
 
   val actorSystem: ActorSystem = ActorSystem("event-processor")
+  val ruleEngineActor: ActorRef =  actorSystem.actorOf(FromConfig.props(Props[RuleEngine]), "rule-engine")
 
-  val ruleEngineActor: ActorRef = actorSystem.actorOf(Props[RuleEngine], "rule-engine")
-
-  ruleEngineActor ! DeviceData("1", 200)
-  ruleEngineActor ! DeviceData("1", 200)
-  ruleEngineActor ! DeviceData("1", 200)
-
-  ruleEngineActor ! InsertRule(RuleDescription("1", 200))
+  implicit val timeout = Timeout(1 seconds)
 
   Thread.sleep(1000)
+  ruleEngineActor ! ConsistentHashableEnvelope(DeviceData("1", 200), "1")
+  Await.ready(ruleEngineActor ? ConsistentHashableEnvelope(InsertRule(RuleDescription("1", 200)), "1"), 1 seconds)
 
-  ruleEngineActor ! DeviceData("1", 200)
-  ruleEngineActor ! DeviceData("1", 201)
-  ruleEngineActor ! DeviceData("2", 201)
+  ruleEngineActor ! ConsistentHashableEnvelope(DeviceData("1", 200), "1")
+  ruleEngineActor ! ConsistentHashableEnvelope(DeviceData("1", 201), "1")
+  Await.ready(ruleEngineActor ? ConsistentHashableEnvelope(InsertRule(RuleDescription("2", 200)), "2"), 1 seconds)
+
+  ruleEngineActor ! ConsistentHashableEnvelope(DeviceData("2", 201), "2")
 }
